0fd881f165b1732d67c17b3c8b87208f2e4b479d
naarad-muni <naarad.muni@protonmail.com>
2023-04-20 10:57:19 +0530

    Solved DP problem for stock buy and sell

---
+++ StockProblem.java
@@ -0,0 +1,25 @@
+/**
+ * <a href="https://leetcode.com/problems/best-time-to-buy-and-sell-stock/">Problem statement link</a>
+ * Runtime Complexity = O(n)
+ * Spacetime complexity = O(1)
+ */
+public class StockProblem {
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 2, 5, 3, 6, 4, 1, 4}));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int maxProfit = 0;
+
+        for (int i = 0, j = 1; j < prices.length; j++) {
+            if (prices[i] > prices[j]) {
+                i = j;
+            } else if (prices[i] < prices[j]) {
+                maxProfit = Math.max(prices[j] - prices[i], maxProfit);
+            }
+        }
+
+        return maxProfit;
+    }
+}

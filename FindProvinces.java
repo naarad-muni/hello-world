b4c3a1b7a36010026cf4aca82c9c264e1ec72bfa
naarad-muni <naarad.muni@protonmail.com>
2023-05-08 19:47:36 +0530

    Added code for graph practise question

---
+++ FindProvinces.java
@@ -0,0 +1,71 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This is a leetcode graph practise question, involving disjoint set.
+ **/
+public class FindProvinces {
+    int[] root;
+
+    public void initialiseRoot(final int length) {
+        root = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            root[i] = i;
+        }
+    }
+
+    public void union(int i, int j) {
+        int previousRoot = root[j];
+        root[j] = root[i];
+
+        for (int k = 0; k < root.length; k++) {
+            if (root[k] == previousRoot) {
+                root[k] = root[i];
+            }
+        }
+    }
+
+    public int findCircleNum(int[][] isConnected) {
+        int length = isConnected[0].length;
+        initialiseRoot(length);
+
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (isConnected[i][j] == 1) {
+                    union(i, j);
+                }
+            }
+        }
+
+        Map<Integer, Boolean> provincesTracker = new HashMap<>();
+
+        for (int i = 0; i < root.length; i++) {
+            provincesTracker.put(root[i], true);
+        }
+
+        return provincesTracker.size();
+    }
+
+    public static void main(String[] args) {
+        FindProvinces provinces = new FindProvinces();
+        System.out.println(
+                provinces.findCircleNum(new int[][]{
+                        {1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+                        {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
+                        {0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+                        {0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0},
+                        {1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0},
+                        {0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1},
+                        {0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1}
+                }));
+    }
+}
--- MinimumDayToEatOranges.java
+++ MinimumDayToEatOranges.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * <a href="https://leetcode.com/problems/minimum-number-of-days-to-eat-n-oranges/">problem statement link</a>
  * <br />
@@ -6,7 +9,7 @@
 public class MinimumDayToEatOranges {
     public static void main(String[] args) {
         MinimumDayToEatOranges a = new MinimumDayToEatOranges();
-        System.out.println(a.minDays(4));
+        System.out.println(a.minDays(61455274));
     }
 
     public int minDays(int n) {
@@ -15,27 +18,35 @@ public class MinimumDayToEatOranges {
         } else if (n == 2 || n == 3) {
             return 2;
         }
-        int[] oranges = new int[n + 1];
 
-        oranges[0] = 0;
-        oranges[1] = 1;
-        oranges[2] = 2;
-        oranges[3] = 2;
+        final Map<Integer, Integer> orangesToMinimumDays = new HashMap<>();
+        orangesToMinimumDays.put(0, 0);
+        orangesToMinimumDays.put(1, 1);
+        orangesToMinimumDays.put(2, 2);
+        orangesToMinimumDays.put(3, 2);
 
-        for (int i = 4; i <= n; i++) {
-            int minCost = 1 + oranges[i - 1];
+        return calculateMinCost(n, orangesToMinimumDays);
+    }
 
-            if (i % 2 == 0) {
-                minCost = Math.min(minCost, 1 + oranges[i - (i / 2)]);
-            }
-
-            if (i % 3 == 0) {
-                minCost = Math.min(minCost, 1 + oranges[i - (2 * (i / 3))]);
-            }
-
-            oranges[i] = minCost;
+    public int calculateMinCost(int n, final Map<Integer, Integer> orangeToMinimumDay) {
+        if (orangeToMinimumDay.containsKey(n)) {
+            return orangeToMinimumDay.get(n);
         }
 
-        return oranges[n];
+        int minimumDays = n;
+
+        if (n % 3 == 0) {
+            minimumDays = Math.min(minimumDays, 1 + calculateMinCost(n - (2 * (n / 3)), orangeToMinimumDay));
+        }
+
+        if (n % 2 == 0) {
+            minimumDays = Math.min(minimumDays, 1 + calculateMinCost(n - (n / 2), orangeToMinimumDay));
+        }
+
+        minimumDays = Math.min(minimumDays, 1 + calculateMinCost(n - 1, orangeToMinimumDay));
+
+        orangeToMinimumDay.put(n, minimumDays);
+
+        return minimumDays;
     }
 }
